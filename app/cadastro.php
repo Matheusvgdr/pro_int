f2bfd014e434c272c8f852a05aa543d7ae3a7ff0
matheus victor <matheusvictorgdr15@gmail.com>
2022-03-21 22:17:35 -0300

    Cadastro e login finalizados

--- app/cadastro.php
+++ app/cadastro.php
@@ -2,9 +2,10 @@
 
 require_once("conexao.php");
 
-if($_POST["nome"] && $_POST["cidade"] && $_POST["idade"] && $_POST["pokemon1"] && $_POST["tipo1"] && $_POST["pokemon2"] && $_POST["tipo2"] && $_POST["pokemon3"] && $_POST["tipo3"]){
+if($_POST["nome"] && $_POST["senha"] && $_POST["cidade"] && $_POST["idade"] && $_POST["pokemon1"] && $_POST["tipo1"] && $_POST["pokemon2"] && $_POST["tipo2"] && $_POST["pokemon3"] && $_POST["tipo3"]){
 
     $nome = $_POST["nome"];
+    $senha = $_POST["senha"];
     $cidade = $_POST["cidade"];
     $idade = $_POST["idade"];
     $pokemon1 = $_POST["pokemon1"];
@@ -15,11 +16,12 @@ if($_POST["nome"] && $_POST["cidade"] && $_POST["idade"] && $_POST["pokemon1"] &
     $tipo3 = $_POST["tipo3"];
 
 
-$query = $conecta->prepare("INSERT INTO tb_cadastro(nome, cidade, idade, pokemon1, tipo1, pokemon2, tipo2, pokemon3, tipo3) VALUES (:NOME, :CIDADE, :IDADE, :POKEMON1, :TIPO1, :POKEMON2, :TIPO2, :POKEMON3, :TIPO3)");
+$query = $conecta->prepare("INSERT INTO tb_cadastro(nome, senha, cidade, idade, pokemon1, tipo1, pokemon2, tipo2, pokemon3, tipo3) VALUES (:NOME, :SENHA, :CIDADE, :IDADE, :POKEMON1, :TIPO1, :POKEMON2, :TIPO2, :POKEMON3, :TIPO3)");
 
 $query->execute(array(
 
     ':NOME'=> $nome,
+    ':SENHA'=> $senha,
     ':CIDADE'=> $cidade,
     ':IDADE'=> $idade,
     ':POKEMON1'=> $pokemon1,
@@ -29,10 +31,7 @@ $query->execute(array(
     ':POKEMON3'=> $pokemon3,
     ':TIPO3'=> $tipo3
 ));
-
-echo "Por favor de certo";
-}else{
-    echo "não funfou";
+   
+    echo '<script>alert("Cadastro feito com sucesso");</script>';
 }
-
 ?>
---
+++ app/login.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once("conexao.php");
+
+$query = $conecta->prepare("SELECT * FROM tb_cadastro WHERE nome = :NOME AND senha = :SENHA");
+
+$nome = $_POST["nome"];
+$senha = $_POST["senha"];
+
+$query->execute(array(
+
+    ':NOME' => $nome,
+    ':SENHA' => $senha
+
+));
+
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
+
+if(count($users) <= 0){
+
+    echo "Nome ou senha não encontrados";
+
+}else{
+
+    $userValid = $users[0];
+
+    session_start();
+
+    $_SESSION['nome'] = $userValid['nome'];
+    $_SESSION['senha'] = $userValid['senha'];
+
+    $_SESSION["login"] = true;
+
+    header('Location: redireciona.php');
+
+}
+
+
+
+
+
+
+?>
